c46d599aa9518da6d28ebcf44ae43e09d4b12612
Moses <moses_hamon@bigpond.com>
2018-08-04 09:46:03 +1000

    Added JTable and MyModel Class File

---
+++ src/traffic_monitor_application_v1/MyModel.java
@@ -0,0 +1,67 @@
+
+package traffic_monitor_application_v1;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author Moe
+ */
+public class MyModel extends AbstractTableModel
+    {
+        ArrayList<Object[]> al;
+
+        // the headers
+        String[] header;
+
+        // constructor 
+        MyModel(ArrayList<Object[]> obj, String[] header)
+        {
+            // save the header
+            this.header = header;
+            // and the data
+            al = obj;
+        }
+
+        // method that needs to be overload. The row count is the size of the ArrayList
+
+        public int getRowCount()
+        {
+            return al.size();
+        }
+
+        // method that needs to be overload. The column count is the size of our header
+        public int getColumnCount()
+        {
+            return header.length;
+        }
+
+        // method that needs to be overload. The object is in the arrayList at rowIndex
+        public Object getValueAt(int rowIndex, int columnIndex)
+        {
+            return al.get(rowIndex)[columnIndex];
+        }
+
+        // a method to return the column name 
+        public String getColumnName(int index)
+        {
+            return header[index];
+        }
+
+        // a method to add a new line to the table
+        
+        //Need to Fix-----------------------------------------
+        
+//        void add(String word1, String word2)
+//        {
+//            // make it an array[2] as this is the way it is stored in the ArrayList
+//            // (not best design but we want simplicity)
+//            String[] str = new String[2];
+//            str[0] = word1;
+//            str[1] = word2;
+//            al.add(str);
+//            // inform the GUI that I have change
+//            fireTableDataChanged();
+//        }
+    }
--- src/traffic_monitor_application_v1/Traffic_Monitor_Application_v1.java
+++ src/traffic_monitor_application_v1/Traffic_Monitor_Application_v1.java
@@ -1,7 +1,6 @@
 //This is a test
 package traffic_monitor_application_v1;
 
-
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
@@ -24,109 +23,152 @@ import java.awt.*;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 
-
 /**
  *
  * @author Moses
  */
 public class Traffic_Monitor_Application_v1 extends JFrame implements ActionListener
 {
-    
-    private JButton btnSortLocation, btnSortVehicleNumber, btnSortVelocity, btnExit, btnPreOrderDisplay, btnPreOrderSave, btnInOrderDisplay,  btnInOrderSave, btnPostOrderDisplay, btnPostOrderSave, btnBinaryTreeDisplay ;
+
+    private JButton btnSortLocation, btnSortVehicleNumber, btnSortVelocity, btnExit, btnPreOrderDisplay, btnPreOrderSave, btnInOrderDisplay, btnInOrderSave, btnPostOrderDisplay, btnPostOrderSave, btnBinaryTreeDisplay;
     private JTextArea txtLinkedList, txtBinaryTreeList;
     private JPanel pnlTrafficData, pnlInformation;
     private JLabel lblTitle, lblDataHeading, lblPreOrder, lblInOrder, lblPostOrder, lblLinkedList, lblBinaryTree;
     private JTable tblTrafficData;
+    private MyModel trafficModel;
 
     public static void main(String[] args)
     {
         JFrame myFrame = new Traffic_Monitor_Application_v1();
-        
-        myFrame.setSize(1000,700);
+
+        myFrame.setSize(1000, 700);
         myFrame.setLocationRelativeTo(null);
         myFrame.setResizable(true);
         myFrame.setVisible(true);
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    
+
     public Traffic_Monitor_Application_v1()
     {
         setTitle("Traffic Monitor Application");
-        
+
         SpringLayout TrafficLayout = new SpringLayout();
         setLayout(TrafficLayout);
-        
+
         displayLabels(TrafficLayout);
         displayButtons(TrafficLayout);
         displayTextFields(TrafficLayout);
         displayJTable(TrafficLayout);
     }
-    
-    private void displayLabels(SpringLayout layout){
-        
+
+    private void displayLabels(SpringLayout layout)
+    {
         lblTitle = LibraryComponents.LocateAJLabel(this, layout, "Monitoring Office", 300, 0);
         lblTitle.setFont(new Font("Serif", Font.PLAIN, 50));
-        
         lblDataHeading = LibraryComponents.LocateAJLabel(this, layout, "Traffic Monitoring Data", 100, 75);
-        
-        lblPreOrder= LibraryComponents.LocateAJLabel(this, layout, "Pre-Order", 50, 575);
-        setupLabel(lblPreOrder);
-        
+        lblPreOrder = LibraryComponents.LocateAJLabel(this, layout, "Pre-Order", 50, 575);
         lblInOrder = LibraryComponents.LocateAJLabel(this, layout, "In-Order", 400, 575);
-        setupLabel(lblInOrder);
-        
         lblPostOrder = LibraryComponents.LocateAJLabel(this, layout, "Post-Order", 800, 575);
-        setupLabel(lblPostOrder);
-        
         lblLinkedList = LibraryComponents.LocateAJLabel(this, layout, "Linked List:", 5, 320);
         lblBinaryTree = LibraryComponents.LocateAJLabel(this, layout, "Binary Tree:", 5, 445);
-        
     }
+
     private void displayButtons(SpringLayout layout)
     {
         btnPreOrderDisplay = LibraryComponents.LocateAJButton(this, this, layout, "Display", 50, 610, 75, 35);
         btnPreOrderSave = LibraryComponents.LocateAJButton(this, this, layout, "Save", 125, 610, 75, 35);
-        
         btnInOrderDisplay = LibraryComponents.LocateAJButton(this, this, layout, "Display", 400, 610, 75, 35);
-        btnInOrderSave  = LibraryComponents.LocateAJButton(this, this, layout, "Save", 475, 610, 75, 35);
-        
+        btnInOrderSave = LibraryComponents.LocateAJButton(this, this, layout, "Save", 475, 610, 75, 35);
         btnPostOrderDisplay = LibraryComponents.LocateAJButton(this, this, layout, "Display", 800, 610, 75, 35);
         btnPostOrderSave = LibraryComponents.LocateAJButton(this, this, layout, "Save", 875, 610, 75, 35);
-        
         btnExit = LibraryComponents.LocateAJButton(this, this, layout, "Exit", 750, 300, 200, 25);
         btnSortLocation = LibraryComponents.LocateAJButton(this, this, layout, "Location", 190, 300, 90, 25);
         btnSortVehicleNumber = LibraryComponents.LocateAJButton(this, this, layout, "Vehicle #", 280, 300, 90, 25);
         btnSortVelocity = LibraryComponents.LocateAJButton(this, this, layout, "Velocity", 370, 300, 80, 25);
     }
-private void setupLabel(JLabel label){
-    label.setBorder(new LineBorder(Color.BLACK));
-    label.setPreferredSize(new Dimension(150, 35));
-    label.setOpaque(true);
-    label.setBackground(Color.GREEN);
-    label.setHorizontalAlignment(JLabel.CENTER);
-    label.setVerticalAlignment(JLabel.CENTER);
-}
-private void displayTextFields(SpringLayout layout){
-    
-    txtLinkedList = LibraryComponents.LocateAJTextArea(this, layout, txtLinkedList, 5, 340, 5, 108);
-    
-   txtBinaryTreeList = LibraryComponents.LocateAJTextArea(this, layout, txtBinaryTreeList, 5, 465, 5, 108);
-}
-private void displayJTable(SpringLayout layout){
-    tblTrafficData = new JTable();
-    add(tblTrafficData);
-    JScrollPane tablePane = new JScrollPane(tblTrafficData);
-    layout.putConstraint(SpringLayout.WEST, tablePane, 50, SpringLayout.WEST, this);
-    layout.putConstraint(SpringLayout.NORTH, tablePane, 50, SpringLayout.NORTH, this);
-    this.add(tablePane);
-}
 
+    private void setupLabels()
+    {
+        setupLabel(lblPreOrder, 150, 35, Color.GREEN);
+    }
+
+    private void setupLabel(JLabel label, int x, int y, Color color)
+    {
+        label.setBorder(new LineBorder(Color.BLACK));
+        label.setPreferredSize(new Dimension(x, y));
+        label.setOpaque(true);
+        label.setBackground(color);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.CENTER);
+    }
+
+    private void displayTextFields(SpringLayout layout)
+    {
+        txtLinkedList = LibraryComponents.LocateAJTextArea(this, layout, txtLinkedList, 5, 340, 5, 108);
+        txtBinaryTreeList = LibraryComponents.LocateAJTextArea(this, layout, txtBinaryTreeList, 5, 465, 5, 108);
+    }
+
+    private void displayJTable(SpringLayout layout)
+    {
+        // Create a panel to hold all other components
+        JPanel pnlTrafficData = new JPanel();
+        pnlTrafficData.setLayout(new BorderLayout());
+        add(pnlTrafficData);
+
+        // Create column names
+        String columnNames[] =
+        {
+            "Word 1", "Word 2"
+        };
+
+        // Create some data
+        ArrayList<Object[]> dataValues = new ArrayList();
+        dataValues.add(new Object[]
+        {
+            "Yes", "No"
+        });
+        dataValues.add(new Object[]
+        {
+            "Hi", "there"
+        });
+        dataValues.add(new Object[]
+        {
+            "True", "False"
+        });
+        dataValues.add(new Object[]
+        {
+            "Cat", "Dog"
+        });
+
+        // constructor of JTable model
+        trafficModel = new MyModel(dataValues, columnNames);
+
+        // Create a new table instance
+        tblTrafficData = new JTable(trafficModel);
+
+        // Configure some of JTable's paramters
+        tblTrafficData.isForegroundSet();
+        tblTrafficData.setShowHorizontalLines(false);
+        tblTrafficData.setRowSelectionAllowed(true);
+        tblTrafficData.setColumnSelectionAllowed(true);
+        add(tblTrafficData);
+
+        // Change the text and background colours
+        tblTrafficData.setSelectionForeground(Color.white);
+        tblTrafficData.setSelectionBackground(Color.red);
+
+        // Add the table to a scrolling pane, size and locate
+        JScrollPane scrollPane = tblTrafficData.createScrollPaneForTable(tblTrafficData);
+        pnlTrafficData.add(scrollPane, BorderLayout.CENTER);
+        pnlTrafficData.setPreferredSize(new Dimension(172, 115));
+        layout.putConstraint(SpringLayout.WEST, pnlTrafficData, 280, SpringLayout.WEST, this);
+        layout.putConstraint(SpringLayout.NORTH, pnlTrafficData, 40, SpringLayout.NORTH, this);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e)
     {
-        
+
     }
-    
 
 }
